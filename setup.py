1baaad06c4878f0662afc6ac04e429b6dfbba605
Sebitas <87529848+SebasCouto@users.noreply.github.com>
2026-03-30 11:18:23 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tech_red_qa',
+    name='sebco-labs-layer-validation',
     version='0.1',
     packages=find_packages(),  # No es necesario el argumento 'where' porque la estructura coincide ahora
     install_requires=[
